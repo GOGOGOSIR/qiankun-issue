dc6ea6fa732c52b9ca28d1ec62cf7f3a3e441204
eric <eric_wan2021@163.com>
2023-03-18 11:00:05 +0800

    fix: 修复跳转子应用用路由懒加载异常

--- packages/qiankun/micro-vue2/src/public-path.js
+++ packages/qiankun/micro-vue2/src/public-path.js
@@ -1,5 +1,6 @@
+/* eslint-disable no-undef */
 if (window.__POWERED_BY_QIANKUN__) {
-  window.__webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__
+  __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__
   console.log(window.__webpack_public_path__)
 }
 
